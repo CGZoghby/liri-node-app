083134f97f92004e6cb92b34af6fa9a767fc886c
Christopher Gregory Zoghby <cgzoghby@gmail.com>
2018-04-09 11:11:47 -0400

    super not-DRY solution

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//Various required node packages and files
 require("dotenv").config();
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
@@ -21,13 +22,15 @@ function commandCall(command, stuff) {
     } else if (command === "do-what-it-says") {
         wildCard(stuff);
     } else {
-        console.log("Command not recognized");
+        console.log("Command not recognized, try calling: " + "\n" +
+            "movie-this, spotify-this-song, my-tweets, or do-what-it-says");
     }
 }
 
 //------------------------MOVIE API CALL-----------------------------------------------------
 function movieSearch(params) {
-    // Grab or assemble the movie name and store it in a variable called "movieName"
+
+    // Function which assembles the movie name and returns it
     function buildMovieTitle(input) {
         var args = ""; //initialize an arguments string that will become the fully assembled movieName passed into queryURL
         for (i = 0; process.argv.length - 3 > i; i++) {
@@ -39,11 +42,17 @@ function movieSearch(params) {
         }
         return args;
     };
-    //build the movie title based on user input
+
+    //build the movie title based on user input, if none provided, default to Mr. Nobody
     var movieName = buildMovieTitle(process.argv);
+    if (movieName === "") {
+        movieName = "Mr.+Nobody";
+        console.log("No movie title was provided, so a default was used.")
+    }
+
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=14a66ffa";
-    //console.log(queryUrl); I know this works to commented out
+    //console.log(queryUrl); I know this works so commented out
     request(queryUrl, function (error, response, body) {
         // If the request is successful
         if (!error && response.statusCode === 200) {
@@ -51,11 +60,11 @@ function movieSearch(params) {
             console.log("Title: " + JSON.parse(body).Title + "\n" +
                 "Year release: " + JSON.parse(body).Year + "\n" +
                 "IMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\n" +
-                "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n" + 
+                "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
                 "Country(ies) of Production: " + JSON.parse(body).Country + "\n" +
                 "Languages: " + JSON.parse(body).Language + "\n" +
                 "Plot: " + JSON.parse(body).Plot + "\n" +
-                "Actors: " + JSON.parse(body).Actors + "\n");
+                "Actors: " + JSON.parse(body).Actors);
         };
     })
 }
@@ -75,6 +84,7 @@ function twitSearch(params) {
 //-----------------------------------SPOTIFY API CALL--------------------------------
 function songSearch(params) {
 
+    //function which assemble the song title and returns it
     function buildSongTitle(input) {
         var args = ""; //initialize an arguments string that will become the fully assembled songName passed into queryURL
         for (i = 0; process.argv.length - 3 > i; i++) {
@@ -87,8 +97,14 @@ function songSearch(params) {
         return args;
     };
 
+    //Build the song title, if no song is given, search using a default songName
     var songName = buildSongTitle(process.argv);
+    if (songName === "") {
+        songName = "The Sign";
+        console.log("No song input provided, so a default was used.")
+    }
 
+    //then pass the songName to the spotify search request
     spotify.search({ type: 'track', query: songName }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
@@ -117,14 +133,63 @@ function wildCard(params) {
         if (error) {
             return console.log(error);
         }
-        // We will then print the contents of data
-        console.log(data);
         // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
         var orders = dataArr[0];
-        var otherStuff = dataArr[1];
+        for (var i = 1; i < dataArr.length; i++) {
+            var otherStuff = "";
+            otherStuff += dataArr[i];
+        }
         console.log(otherStuff);
-        commandCall(orders, otherStuff);
+        //---------------------------------------------------wildcard spotify call------------------------------------------
+        if (orders === "spotify-this-song") {
+            if (otherStuff === "") {
+                otherStuff = "The Sign";
+                console.log("No song input provided, so a default was used.")
+            }
+            spotify.search({ type: 'track', query: otherStuff }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log("Artist: " + data["tracks"]["items"][0].artists[0].name);
+                console.log("Song title: " + data["tracks"]["items"][0].name)
+                console.log("Preview link: " + data["tracks"]["items"][0].preview_url)
+                console.log("Album the song is on: " + data["tracks"]["items"][0]["album"].name)
+                //for (var i = 0; i < data["tracks"]["items"].length; i++) {
+                //    console.log(data["tracks"]["items"][i].artists[0].name)
+                //    console.log(data["tracks"]["items"][i].name)
+                //    console.log(data["tracks"]["items"][i].preview_url)
+                //    console.log(data["tracks"]["items"][i]["album"].name)
+                //}
+            });
+        }
+        //------------------------------------------wildcard movie call---------------------------------------------------
+        if (orders === "movie-this") {
+            if (otherStuff === "") {
+                otherStuff = "Mr.+Nobody";
+                console.log("No movie title was provided, so a default was used.")
+            }
+            // Then run a request to the OMDB API with the movie specified
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=14a66ffa";
+            //console.log(queryUrl); I know this works so commented out
+            request(queryUrl, function (error, response, body) {
+                // If the request is successful
+                if (!error && response.statusCode === 200) {
+                    // Then log the Release Year for the movie
+                    console.log("Title: " + JSON.parse(body).Title + "\n" +
+                        "Year release: " + JSON.parse(body).Year + "\n" +
+                        "IMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\n" +
+                        "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
+                        "Country(ies) of Production: " + JSON.parse(body).Country + "\n" +
+                        "Languages: " + JSON.parse(body).Language + "\n" +
+                        "Plot: " + JSON.parse(body).Plot + "\n" +
+                        "Actors: " + JSON.parse(body).Actors);
+                };
+            })
+        }
+        if (orders === "my-tweets") {
+            commandCall(orders);
+        };
     });
 }
 //------------------------end random.txt file------------------------
